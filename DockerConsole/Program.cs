92765cc9db0a3c2ca5c893ec192d2ea7ad5530c4
serkanyurttapan_ <serkan.yurttapan@asistbt.com.tr>
2020-12-30 00:24:02 +0300

    Add project files.

---
+++ DockerConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DockerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
